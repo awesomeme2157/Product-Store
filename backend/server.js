2852cb84515d9edb0002b2379162391cac30c78c
Raj Mhetar <raj@Rajs-MacBook-Air.local>
2024-11-01 11:39:17 +0530

    made ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -12,12 +13,22 @@ connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.send("Home Page");
-});
+const __dirname = path.resolve();
+
+// app.get("/", (req, res) => {
+//     return res.end("Home Page");
+// })
 
 // Use the product routes
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+}
+
+app.get("*", (req, res) => {
+    return res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
